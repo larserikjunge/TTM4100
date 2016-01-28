aeb72823fff5c754d9177d8b556b153ecaaa30df
Lars Erik Junge <larserikjunge@gmail.com>
2016-01-28 20:04:40 +0100

    Third commit

--- WebServer/webServer.py
+++ WebServer/webServer.py
@@ -1,13 +1,8 @@
-# Import socket module
 from socket import *    
 
-# Create a TCP server socket
-#(AF_INET is used for IPv4 protocols)
-#(SOCK_STREAM is used for TCP)
-serverSocket = socket(AF_INET, SOCK_STREAM)
 
-# Prepare a server socket
-# FILL IN START
+# Create a TCP server socket
+serverSocket = socket(AF_INET, SOCK_STREAM)
 
 # Assign a port number
 serverPort = 6789
@@ -18,41 +13,27 @@ serverSocket.bind(('', serverPort))
 # Listen to at most 1 connection at a time
 serverSocket.listen(1)
 
-# FILL IN END
 
-# Server should be up and running and listening to the incoming connections
 while True:
 	print 'Ready to serve...'
 	
 	# Set up a new connection from the client
 	connectionSocket, addr = serverSocket.accept()
 	
-	# If an exception occurs during the execution of try clause
-	# the rest of the clause is skipped
-	# If the exception type matches the word after except
-	# the except clause is executed
 	try:
 		# Receives the request message from the client
 		message =  connectionSocket.recv(1024)
-		# Extract the path of the requested object from the message
-		# The path is the second part of HTTP header, identified by [1]
+		# Extract the path from the message
 		filepath = message.split()[1]
 		
-		# Because the extracted path of the HTTP request includes 
-		# a character '\', we read the path from the second character 
+		# Read the file into a buffer
 		f = open(filepath[1:])
-		
-		# Read the file "f" and store the entire content of the requested file in a temporary buffer
-		#outputdata = f.read()
-		outputdata = open(f, 'r')
+		outputdata = f.read()
 		
 		# Send the HTTP response header line to the connection socket
-		# Format: "HTTP/1.1 *code-for-successful-request*\r\n\r\n"
-		# FILL IN START		
 		connectionSocket.send("HTTP/1.1 200 OK \r\n\r\n")
- 		# FILL IN END
  		
-		# Send the content of the requested file to the connection socket
+		# Send the requested file to the connection socket
 		for i in range(0, len(outputdata)):  
 			connectionSocket.send(outputdata[i])
 		connectionSocket.send("\r\n")
@@ -61,17 +42,11 @@ while True:
 		connectionSocket.close()
 
 	except IOError:
-		# Send HTTP response message for file not found
-		# Same format as above, but with code for "Not Found"
-		# FILL IN START		
+		# Send HTTP response mesage for file not found
 		connectionSocket.send("HTTP/1.1 404 Not Found \r\n\r\n")
- 		# FILL IN END
 		connectionSocket.send("<html><head></head><body><h1>404 Not Found</h1></body></html>\r\n")
 		
-		# Close the client connection socket
-		# FILL IN START		
 		connectionSocket.close()	
- 		# FILL IN END
 
 serverSocket.close()  
 
